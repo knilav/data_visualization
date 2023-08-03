617ed86b8e4c34a42296bd0c3f2f010f95902f35
knilav <knilav@gmail.com>
2023-08-03 13:51:59 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -56,7 +56,7 @@ with tab1:
       y_axis_label='y')
    
    p.rect(x_center, y_mean, width = x_width, height = y_std, legend_label="Mean", line_width=0, fill_color = "lightgray")
-   p.circle(x, y, legend_label='Trend', line_width=5)
+   p.circle(x, y, legend_label='Data', line_width=5)
    p.line([x_min, x_max], [y_mean, y_mean], legend_label="Mean", line_width=2)
    
    
