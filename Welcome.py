9d980142d58759bb8d531a20cb2eb9a3484974e6
knilav <knilav@gmail.com>
2023-08-03 13:02:33 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -9,58 +9,58 @@ tab1, tab2 = st.tabs(["Plotting", "Map"])
 
 with tab1:
    st.header("Plotting")
-  file_name_list = []
-  for i in os.listdir():
+   file_name_list = []
+   for i in os.listdir():
     if i.endswith(".csv"):
       file_name_list.append(i)
-  
-  #st.write(file_name_list)
+   
+   #st.write(file_name_list)
     
-  location = st.selectbox("Select location", file_name_list)
-  
-  
-  df = pd.read_csv(location)
-  st.dataframe(df)
-  
-  el_list = df.columns.tolist()[27:80]
-  
-  col1, col2 = st.columns([1, 4])
-  
-  col1.subheader("Data selection")
-  x_axis = col1.selectbox("Select element#1", el_list, index = 0)
-  y_axis = col1.selectbox("Select element#2", el_list, index = 0)
-  
-  x = df[x_axis]/10000
-  y = df[y_axis]/10000
-  y_mean = np.mean(y)
-  x_min = np.min(x)
-  x_max = np.max(x)
-  
-  x_center = 0.5*(x_min + x_max)
-  x_width = x_max - x_min
-  
-  std_level = col1.radio("Select std level", ('1', '2', '3'))  
-  if std_level == "1":
+   location = st.selectbox("Select location", file_name_list)
+   
+   
+   df = pd.read_csv(location)
+   st.dataframe(df)
+   
+   el_list = df.columns.tolist()[27:80]
+   
+   col1, col2 = st.columns([1, 4])
+   
+   col1.subheader("Data selection")
+   x_axis = col1.selectbox("Select element#1", el_list, index = 0)
+   y_axis = col1.selectbox("Select element#2", el_list, index = 0)
+   
+   x = df[x_axis]/10000
+   y = df[y_axis]/10000
+   y_mean = np.mean(y)
+   x_min = np.min(x)
+   x_max = np.max(x)
+   
+   x_center = 0.5*(x_min + x_max)
+   x_width = x_max - x_min
+   
+   std_level = col1.radio("Select std level", ('1', '2', '3'))  
+   if std_level == "1":
     y_std = np.std(y) * 1.0
-  if std_level == "2":
+   if std_level == "2":
     y_std = np.std(y) * 2.0
-  if std_level == "3":
+   if std_level == "3":
     y_std = np.std(y) * 3.0
-  
-  col2.subheader("Data chart")
-  
-  
-  p = figure(
+   
+   col2.subheader("Data chart")
+   
+   
+   p = figure(
       title='simple line example',
       x_axis_label='x',
       y_axis_label='y')
-  
-  p.rect(x_center, y_mean, width = x_width, height = y_std, legend_label="Mean", line_width=0, fill_color = "lightgray")
-  p.circle(x, y, legend_label='Trend', line_width=5)
-  p.line([x_min, x_max], [y_mean, y_mean], legend_label="Mean", line_width=2)
-  
-  
-  col2.bokeh_chart(p, use_container_width=True)   
+   
+   p.rect(x_center, y_mean, width = x_width, height = y_std, legend_label="Mean", line_width=0, fill_color = "lightgray")
+   p.circle(x, y, legend_label='Trend', line_width=5)
+   p.line([x_min, x_max], [y_mean, y_mean], legend_label="Mean", line_width=2)
+   
+   
+   col2.bokeh_chart(p, use_container_width=True)   
 
 with tab2:
    st.header("Map")
