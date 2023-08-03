14df3b3f1e3cde6f1a56f5bce8c97663709da450
knilav <knilav@gmail.com>
2023-08-03 09:40:59 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -1,11 +1,18 @@
 import streamlit as st
 import pandas as pd
+import os
 
+file_name_list = []
+for i in os.listdir():
+  if i.endswith(".csv"):
+    file_name_list.append(i)
+
+st.write(file_name_list)
+  
 st.write("Hello world!")
 
 df = pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
-el_list = df.columns.to_list()[27:80]
-
-st.selectbox("Select element", el_list)
+el_list = df.columns.tolist()[27:80]
+x_axis = st.selectbox("Select element", el_list)
